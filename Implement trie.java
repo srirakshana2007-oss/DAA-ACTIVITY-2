89c65e592a29591c26d77bf2cdf54a692c03393a
srirakshana2007-oss <srirakshana2007@gmail.com>
2026-09-08 20:41:08 +0530

    Create Implement trie.java

---
+++ Implement trie.java
@@ -0,0 +1,63 @@
+// Link: https://leetcode.com/problems/implement-trie-prefix-tree/
+
+class Trie {
+
+    class TrieNode {
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd;
+    }
+
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode current = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                current.children[index] = new TrieNode();
+            }
+
+            current = current.children[index];
+        }
+
+        current.isEnd = true;
+    }
+
+    public boolean search(String word) {
+        TrieNode current = root;
+
+        for (char c : word.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return current.isEnd;
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode current = root;
+
+        for (char c : prefix.toCharArray()) {
+            int index = c - 'a';
+
+            if (current.children[index] == null) {
+                return false;
+            }
+
+            current = current.children[index];
+        }
+
+        return true;
+    }
+}
